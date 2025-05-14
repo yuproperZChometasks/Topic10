45fae417dbc318469177f5bdbb661ec857497732
yuproper <yuproper@gmail.com>
2025-05-15 03:01:58 +0700

    add DJ04
    add DJ04

--- dj04_forms/forms.py
+++ dj04_forms/forms.py
@@ -1,8 +1,13 @@
 from .models import News_post
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
 class New_postForm(ModelForm):
 	class Meta:
 		model = News_post
 		fields = ['title', 'short_description', 'text', 'pub_date'] 
-	
+		widgets = {
+			'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости'}),
+			'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
+			'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости'}),
+			'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации'}),
+		}
--- dj04_forms/views.py
+++ dj04_forms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect  
 from .models import News_post
 from .forms import New_postForm
 
@@ -19,7 +19,17 @@ def news(request):
 # Прописываем словарь для передачи информации в html-шаблон.
 	news = News_post.objects.all()
 	return render(request, 'dj04_forms/news.html', {'news': news})
-	
+
 def create_news(request):
-	form = New_postForm()
-	return render(request, 'dj04_forms/add_new_post.html', {'form': form})
+    error = ''
+    if request.method == 'POST':
+        form = New_postForm(request.POST)  # Сюда сохранится информация от пользователя.
+        if form.is_valid():  # 
+            form.save()
+            return redirect('news_dj04')  # Перенаправление после успешного создания
+        else:
+            error = "Данные были заполнены некорректно" 
+    else:
+        form = New_postForm()  # Создание новой формы только при GET-запросе
+
+    return render(request, 'dj04_forms/add_new_post.html', {'form': form, 'error': error})
